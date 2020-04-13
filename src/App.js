91bad9a1216c4f3d6707c8ce99d330d6cabb56cc
Can Muhammet GULER <canmuhammetguler@Cans-MacBook-Pro.local>
2020-04-13 02:20:52 -0500

    react drag and drop app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null,
+    };
+  }
+
+  onChangeHandler = (event) => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    });
+  };
+
+  onClickHandler = () => {
+    const data = new FormData();
+    data.append("file", this.state.selectedFile);
+    axios.post("http://localhost:9000/upload", data, {}).then((res) => {
+      console.log(res.statusText);
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <form method="post" action="#" id="#">
+              <div className="form-group files">
+                <h1>File Uploader</h1>
+                <input
+                  type="file"
+                  name="file"
+                  className="form-control"
+                  onChange={this.onChangeHandler}
+                ></input>
+                <button
+                  type="button"
+                  className="btn btn-success btn-block"
+                  onClick={this.onClickHandler}
+                >
+                  Upload
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
